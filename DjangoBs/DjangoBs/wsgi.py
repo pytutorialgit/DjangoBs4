6dfbf4d50c64c098a5bc1239a90a86ee6f56b157
Pytutorial <76822075+pytutorialgit@users.noreply.github.com>
2020-12-31 20:33:00 +0100

    Add files via upload

---
+++ DjangoBs/DjangoBs/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for DjangoBs project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoBs.settings')
+
+application = get_wsgi_application()
